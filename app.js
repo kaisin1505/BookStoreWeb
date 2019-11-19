8556021f66d45fdb4ea90dd617e326728cd8c09d
DTin98 <1612699@student.hcmus.edu.vn>
2019-11-19 12:49:37 +0700

    Edited Port for deploying

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.use(bodyParser.urlencoded({
 
 app.use(handleLayout);
 app.use('/', accountController);
-app.listen(3000, () => {
-    console.log('Site  running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    //console.log('Site  running on port 3000');
 });
 
 
